1857ca2862d962e9ac907e065f4f1569a6c1ce68
cadeau345 <cadeau200510@gmail.com>
2025-06-14 20:18:02 +0300

    Add files via upload

---
+++ firebase_confing.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCV13McNIh6EaO_hMsaNHZNTZbZPGqiheQ",
+    authDomain: "raya-project-ff96a.firebaseapp.com",
+    projectId: "raya-project-ff96a",
+    storageBucket: "raya-project-ff96a.firebasestorage.app",
+    messagingSenderId: "378224584383",
+    appId: "1:378224584383:web:664b0ac5e56e23c47d080a",
+    measurementId: "G-3W5S5DZDM1"
+};
